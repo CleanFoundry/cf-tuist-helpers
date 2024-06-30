10da26aeefb6b206653dd9b5765edf28d0491afc
Connor Neville <connor.neville16@gmail.com>
2024-06-30 10:44:10 -0400

    More CFTarget+Defaults fixes

--- ProjectDescriptionHelpers/CFTarget/CFTarget+Defaults.swift
+++ ProjectDescriptionHelpers/CFTarget/CFTarget+Defaults.swift
@@ -3,93 +3,97 @@ import ProjectDescription
 public extension CFTarget {
 
     struct Default {
-
-        static func app(
-            name: Name,
-            destinations: Set<Destination>,
-            infoPlist: InfoPlist,
-            internalDependencies: [CFTarget.Name],
-            externalDependencies: [CFExternalTargetName],
-            deploymentTarget: DeploymentTarget,
-            settings: SettingsDictionary = [:],
-            launchArguments: [LaunchArgument] = []
-        ) -> CFTarget {
-            CFTarget(
-                name: name,
-                destinations: destinations,
-                product: .app,
-                bundleID: .default(targetName: name),
-                deploymentTarget: deploymentTarget,
-                infoPlist: infoPlist,
-                sourcePaths: [
-                    .sources(targetName: name)
-                ],
-                resourcePaths: [
-                    .resources(targetName: name)
-                ],
-                internalDependencies: internalDependencies,
-                externalDependencies: externalDependencies,
-                settings: settings,
-                launchArguments: launchArguments
-            )
-        }
-
-        static func framework(
-            name: Name,
-            destinations: Set<Destination>,
-            internalDependencies: [CFTarget.Name],
-            externalDependencies: [CFExternalTargetName],
-            deploymentTarget: DeploymentTarget,
-            includeResources: Bool = false
-        ) -> CFTarget {
-            CFTarget(
-                name: name,
-                destinations: destinations,
-                product: .framework,
-                bundleID: .default(targetName: name),
-                deploymentTarget: deploymentTarget,
-                infoPlist: .default,
-                sourcePaths: [
-                    .sources(targetName: name),
-                ],
-                resourcePaths: includeResources ? [
-                    .resources(targetName: name)
-                ] : [],
-                internalDependencies: internalDependencies,
-                externalDependencies: externalDependencies,
-                settings: [:],
-                launchArguments: []
-            )
-        }
-
-        static func frameworkUnitTest(
-            testing target: CFTarget,
-            internalDependencies: [CFTarget.Name],
-            externalDependencies: [CFExternalTargetName],
-            deploymentTarget: DeploymentTarget
-        ) -> CFTarget {
-            let name: CFTarget.Name = .testing(targetName: target.name)
-            return CFTarget(
-                name: name,
-                destinations: target.destinations,
-                product: .frameworkUnitTests,
-                bundleID: .default(targetName: name),
-                deploymentTarget: deploymentTarget,
-                infoPlist: .default,
-                sourcePaths: [
-                    .tests(testTargetName: name),
-                ],
-                resourcePaths: [],
-                internalDependencies: internalDependencies + [target.name],
-                externalDependencies: externalDependencies,
-                settings: [:],
-                launchArguments: [
-                    .recordUISnapshots,
-                ]
-            )
-        }
-
+        fileprivate init() { }
     }
     static let `default` = Default()
+
+}
+
+public extension CFTarget.Default {
+
+    func app(
+        name: CFTarget.Name,
+        destinations: Set<CFTarget.Destination>,
+        infoPlist: CFTarget.InfoPlist,
+        internalDependencies: [CFTarget.Name],
+        externalDependencies: [CFExternalTargetName],
+        deploymentTarget: CFTarget.DeploymentTarget,
+        settings: SettingsDictionary = [:],
+        launchArguments: [CFTarget.LaunchArgument] = []
+    ) -> CFTarget {
+        CFTarget(
+            name: name,
+            destinations: destinations,
+            product: .app,
+            bundleID: .default(targetName: name),
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            sourcePaths: [
+                .sources(targetName: name)
+            ],
+            resourcePaths: [
+                .resources(targetName: name)
+            ],
+            internalDependencies: internalDependencies,
+            externalDependencies: externalDependencies,
+            settings: settings,
+            launchArguments: launchArguments
+        )
+    }
+
+    func framework(
+        name: CFTarget.Name,
+        destinations: Set<CFTarget.Destination>,
+        internalDependencies: [CFTarget.Name],
+        externalDependencies: [CFExternalTargetName],
+        deploymentTarget: CFTarget.DeploymentTarget,
+        includeResources: Bool = false
+    ) -> CFTarget {
+        CFTarget(
+            name: name,
+            destinations: destinations,
+            product: .framework,
+            bundleID: .default(targetName: name),
+            deploymentTarget: deploymentTarget,
+            infoPlist: .default,
+            sourcePaths: [
+                .sources(targetName: name),
+            ],
+            resourcePaths: includeResources ? [
+                .resources(targetName: name)
+            ] : [],
+            internalDependencies: internalDependencies,
+            externalDependencies: externalDependencies,
+            settings: [:],
+            launchArguments: []
+        )
+    }
+
+    func frameworkUnitTest(
+        testing target: CFTarget,
+        internalDependencies: [CFTarget.Name],
+        externalDependencies: [CFExternalTargetName],
+        deploymentTarget: CFTarget.DeploymentTarget
+    ) -> CFTarget {
+        let name: CFTarget.Name = .testing(targetName: target.name)
+        return CFTarget(
+            name: name,
+            destinations: target.destinations,
+            product: .frameworkUnitTests,
+            bundleID: .default(targetName: name),
+            deploymentTarget: deploymentTarget,
+            infoPlist: .default,
+            sourcePaths: [
+                .tests(testTargetName: name),
+            ],
+            resourcePaths: [],
+            internalDependencies: internalDependencies + [target.name],
+            externalDependencies: externalDependencies,
+            settings: [:],
+            launchArguments: [
+                .recordUISnapshots,
+            ]
+        )
+    }
 
 }
